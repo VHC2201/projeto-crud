2d6a64acd57abdb63797dc5dc1acb63fb0ba5e5e
VHC2201 <vinicius220100@edu.unipar.br>
2026-04-01 21:34:41 -0300

    feat: configuracao de rotas e controllers resource

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('products.index');
 });
+
+Route::resource('suppliers', SupplierController::class);
+Route::resource('products', ProductController::class);
